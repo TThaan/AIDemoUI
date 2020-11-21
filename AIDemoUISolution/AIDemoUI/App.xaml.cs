45e22674e45cd4bcabedaaf76f11b4f81f94d729
TThaan <tthaan@gmx.de>
2020-11-21 17:02:12 +0100

    startet Wpf project

---
+++ AIDemoUISolution/AIDemoUI/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace AIDemoUI
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ AIDemoUISolution/AIDemoUI/Views/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace AIDemoUI.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
